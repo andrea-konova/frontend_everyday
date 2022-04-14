1d019503820ccb9ecdfff259408c0e4b525f54a9
Andrii Konovalenko <an256.ak@gmail.com>
2022-04-14 14:42:32 +0300

    feat: add header

--- task_2/script.js
+++ task_2/script.js
@@ -1,14 +1,14 @@
-const burgerMenu = () => {
-	const burgerBtn = document.getElementById('burger'),
-		headerMenu = document.querySelector('.navigation'),
-    header = document.querySelector('.header');
+// const burgerMenu = () => {
+// 	const burgerBtn = document.getElementById('burger'),
+// 		headerMenu = document.querySelector('.navigation'),
+//     header = document.querySelector('.header');
 
-  header.addEventListener('click', (e) => {
-		if (e.target.matches(".menu__item--link") || e.target.matches(".burger")) {
-      burgerBtn.classList.toggle("burger--active");
-      headerMenu.classList.toggle('navigation-hide');
-    }
-	});
-};
+//   header.addEventListener('click', (e) => {
+// 		if (e.target.matches(".menu__item--link") || e.target.matches(".burger")) {
+//       burgerBtn.classList.toggle("burger--active");
+//       headerMenu.classList.toggle('navigation-hide');
+//     }
+// 	});
+// };
 
-burgerMenu();
+// burgerMenu();
