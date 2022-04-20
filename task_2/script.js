aba34a5164b3f5a0220e1ac18e13404db5afd611
Andrii Konovalenko <an256.ak@gmail.com>
2022-04-20 21:23:37 +0300

    fix: fix small errors and burger button

--- task_2/script.js
+++ task_2/script.js
@@ -1,10 +1,10 @@
 const burgerMenu = () => {
-	const burgerBtn = document.getElementById('burger'),
+	const burgerBtn = document.querySelector('.burger'),
 		headerMenu = document.querySelector('.burger-wrap'),
     header = document.querySelector('.header');
 
   header.addEventListener('click', (e) => {
-		if (e.target.matches(".menu__item--link") || e.target.matches(".burger") || e.target.matches(".login-button")) {
+		if (e.target.matches(".menu__item--link") || e.target.matches(".burger") || e.target.matches(".burger span") || e.target.matches(".login-button")) {
       burgerBtn.classList.toggle("burger--active");
       headerMenu.classList.toggle('burger-wrap-hide');
     }
