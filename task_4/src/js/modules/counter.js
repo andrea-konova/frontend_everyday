6a665559edc8508ba4ced79d89823502a6ccc9fc
Andrii Konovalenko <an256.ak@gmail.com>
2022-05-24 23:51:51 +0300

    fix: fix counter function

--- task_4/src/js/modules/counter.js
+++ task_4/src/js/modules/counter.js
@@ -1,21 +1,45 @@
 export const counter = () => {
+
+  const data = document.querySelector('.data');
+
   const counters = document.querySelectorAll('.number'),
-    speed = 50;
+    speed = 100;
 
-  counters.forEach(counter => {
-    const updateCount = () => {
-      const target = +counter.getAttribute('data-target'),
-        count = +counter.innerText,
-        i = target / speed;
+  const runCounter = () => {
+    counters.forEach(counter => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-target'),
+          count = +counter.innerText,
+          i = target / speed;
 
-      if (count < target) {
-        counter.innerText = Math.ceil(count + i);
-        setTimeout(updateCount, 1);
-      } else {
-        counter.innerText = target;
+        if (count < target) {
+          counter.innerText = Math.ceil(count + i);
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
       }
-    }
 
-    updateCount();
-  });
+      updateCount();
+    });
+  }
+
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  window.onscroll = () => {
+    let visibility = isElementInViewport(data);
+
+    if (visibility) {
+      runCounter();
+    }
+  }
 }
