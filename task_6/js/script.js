d07057571c022e36f5429a51ead59df09d10c5fa
Andrii Konovalenko <an256.ak@gmail.com>
2022-05-31 19:30:52 +0300

    feat: add script

--- task_6/js/script.js
+++ task_6/js/script.js
@@ -1,12 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const switchBtn = document.querySelector('.switch-btn');
+  const switchBtn = document.querySelector('.switch-btn'),
+    cardsPriceAmount = document.querySelectorAll('.card__price-amount');
 
-  switchBtn.addEventListener('click', (e) => {
+  let percent = 20,
+    switchOn = false;
+
+  const updateCardPrice = () => {
+    cardsPriceAmount.forEach(cardPriceAmount => {
+      let target = cardPriceAmount.getAttribute('data-target');
+      let newPrice;
+
+      if (switchOn) {
+        newPrice = Math.round((target * 12) * ((100 - percent) / 100));
+      } else {
+        newPrice = target;
+      }
+      cardPriceAmount.textContent = newPrice;
+    });
+  }
+
+  switchBtn.addEventListener('click', () => {
     switchBtn.classList.toggle('switch-on');
     if (switchBtn.classList.contains('switch-on')) {
-      console.log('Annually');
+      switchOn = true;
     } else {
-      console.log('Monthly');
+      switchOn = false;
     }
+    updateCardPrice();
   })
+
+  updateCardPrice();
 })
