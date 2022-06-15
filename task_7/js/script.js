0fd9831f44953aaec4422f19ed4d6a19af7e1ad4
Andrii Konovalenko <an256.ak@gmail.com>
2022-06-15 18:49:40 +0300

    fix: tried to fix audio, but without success

--- task_7/js/script.js
+++ task_7/js/script.js
@@ -17,7 +17,8 @@ const body = document.querySelector('body'),
   quoteAuthor = document.querySelector('.quote__author'),
   toggleButton = document.querySelector('.toggle-button'),
   footer = document.querySelector('.footer'),
-  header = document.querySelector('.header');
+  header = document.querySelector('.header'),
+  audioDeception = document.querySelector('.audio-deception');
 
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
@@ -119,15 +120,16 @@ async function getQuotes() {
 
 // audio player
 const audio = new Audio();
+audio.muted = true;
 
 const playAudio = (playNum) => {
   audio.src = playList[playNum].src;
   audio.currentTime = 0;
-  audio.setAttribute('muted', '');
+  audio.muted = false;
   audio.play();
 }
 
-// типо функция показа временной зоны, нужно доделать
+// функция показа временной зоны и гороа
 const showTimeZone = () => {
   const offsetInHours = dayjs().utcOffset() / 60;
 
@@ -136,88 +138,21 @@ const showTimeZone = () => {
 
   let localTimeZone;
 
-  switch (offsetInHours) {
-    case 0:
-      localTimeZone = 'GMT';
-      break;
-    case 1:
-      localTimeZone = 'CET';
-      break;
-    case 2:
-      localTimeZone = 'EET';
-      break;
-    case 3:
-      localTimeZone = 'MSK';
-      break;
-    case 4:
-      localTimeZone = 'UTC+4';
-      break;
-    case 5:
-      localTimeZone = 'UTC+5';
-      break;
-    case 6:
-      localTimeZone = 'UTC+6';
-      break;
-    case 7:
-      localTimeZone = 'UTC+7';
-      break;
-    case 8:
-      localTimeZone = 'AWST';
-      break;
-    case 9:
-      localTimeZone = 'UTC+9';
-      break;
-    case 10:
-      localTimeZone = 'AEST';
-      break;
-    case 11:
-      localTimeZone = 'UTC+11';
-      break;
-    case 12:
-      localTimeZone = 'UTC+12';
-      break;
-    case -1:
-      localTimeZone = 'UTC−1';
-      break;
-    case -2:
-      localTimeZone = 'UTC−2';
-      break;
-    case -3:
-      localTimeZone = 'UTC−3';
-      break;
-    case -4:
-      localTimeZone = 'AST';
-      break;
-    case -5:
-      localTimeZone = 'EST';
-      break;
-    case -6:
-      localTimeZone = 'CST';
-      break;
-    case -7:
-      localTimeZone = 'MST';
-      break;
-    case -8:
-      localTimeZone = 'PST';
-      break;
-    case -9:
-      localTimeZone = 'AKST';
-      break;
-    case -10:
-      localTimeZone = 'HAST';
-      break;
-    case -11:
-      localTimeZone = 'SST';
-      break;
-    case -12:
-      localTimeZone = 'UTC−12';
-      break;
-    default:
-      break;
+  if (offsetInHours > 0) {
+    localTimeZone = '+';
+  } else if (offsetInHours < 0){
+    localTimeZone = '-';
   }
 
-  timeZone.textContent = localTimeZone;
+  timeZone.textContent = `UTC ${localTimeZone}${offsetInHours}`;
   city.textContent = timeZoneTown;
+}
+
+// обмануть браузер
+const trickTheBrowser = () => {
+  setTimeout(() => {
+    audioDeception.click();
+  }, 2000);
 }
 
 // вызов функций
@@ -228,18 +163,26 @@ getQuotes();
 
 // слушиватели событий
 changeQuote.addEventListener('click', getQuotes);
+audioDeception.addEventListener('click', () => {
+  playAudio(playNum)
+})
 
 toggleButton.addEventListener('click', () => {
   toggleFooter();
   changeToggleButton();
 })
 
-// audio.addEventListener("ended", () => {
-//   if (playNum === playList.length - 1) {
-//     playNum = 0;
-//   } else {
-//     playNum++;
-//   }
+audio.addEventListener("ended", () => {
+  if (playNum === playList.length - 1) {
+    playNum = 0;
+  } else {
+    playNum++;
+  }
 
-//   playAudio(playNum);
-// });
+  playAudio(playNum);
+});
+
+
+// window.onload = () => {
+//   trickTheBrowser();
+// };
