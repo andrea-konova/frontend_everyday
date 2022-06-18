a899983a63990afe685628a5110fb0c9fd0f0bf6
Andrii Konovalenko <an256.ak@gmail.com>
2022-06-19 00:34:54 +0300

    feat: add timer path

--- task_8/src/js/modules/timer.js
+++ task_8/src/js/modules/timer.js
@@ -4,12 +4,14 @@ export const timer = () => {
   const timer = document.querySelector('.timer__counter'),
     buttonPomodoro = document.getElementById('button-1'),
     buttonShort = document.getElementById('button-2'),
-    buttonLong = document.getElementById('button-3');
+    buttonLong = document.getElementById('button-3'),
+    timerPathRemaining = document.querySelector('.timer__path-remaining');
 
   // const для режимов работы таймера
   const POMODORO = 1200,
     SHORT_BREAK = 300,
-    LONG_BREAK = 900;
+    LONG_BREAK = 900,
+    FULL_DASH_ARRAY = 283;
 
   // переменные для функций
   let timePassed = 0,
@@ -34,6 +36,27 @@ export const timer = () => {
     return `${minutes}:${seconds}`;
   }
 
+  // функция рвсчета доли начального времени
+  const calculateTimeFraction = () => {
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+  }
+
+  // функция обновления значение свойства dasharray, начиная с 283
+  const setCircleDasharray = () => {
+    const circleDasharray = `${(
+      calculateTimeFraction() * FULL_DASH_ARRAY
+    ).toFixed(0)} 283`;
+
+    timerPathRemaining.setAttribute("stroke-dasharray", circleDasharray);
+  }
+
+  // функция очистить интервал
+  const onTimesUp = () => {
+    clearInterval(timerInterval);
+  }
+
   // функция запуска таймера
   const startTimer = () =>{
     timerInterval = setInterval(() => {
@@ -41,6 +64,11 @@ export const timer = () => {
       timeLeft = TIME_LIMIT - timePassed;
 
       timer.textContent = howTimeLeft(timeLeft);
+      setCircleDasharray();
+
+      if (timeLeft === 0) {
+        onTimesUp();
+      }
     }, 1000);
   }
 
