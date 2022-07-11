e24d4dad739bb009fd5a10a9c2812dfbdc571df9
Andrii Konovalenko <an256.ak@gmail.com>
2022-07-11 14:34:21 +0300

    feat: start task_9

--- task_8/gulp/tasks/copyFavicon.js
+++
@@ -1,4 +0,0 @@
-export const copyFavicon = () => {
-  return app.gulp.src(app.path.src.favicon)
-    .pipe(app.gulp.dest(app.path.build.favicon))
-}
--- task_8/src/js/app.js
+++
@@ -1,7 +0,0 @@
-import * as allFunction from "./modules/function.js";
-import { timer } from "./modules/timer.js";
-import { popups } from "./modules/popups.js";
-
-allFunction.isWebp();
-timer();
-popups();
