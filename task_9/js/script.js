9b9b6f2637344bd1053114b5f91340553b76d882
Andrii Konovalenko <an256.ak@gmail.com>
2022-07-15 13:41:18 +0300

    feat: completed task item a, b, c

--- task_9/js/script.js
+++ task_9/js/script.js
@@ -4,23 +4,38 @@ const addButton = document.querySelector('.todo__add'),
 
 let todoItems = [];
 
+// функция показать новое дело
 const renderTodo = (todo) => {
+  localStorage.setItem('todoItemsRef', JSON.stringify(todoItems));
+
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  if (todo.deleted) {
+    item.remove();
+    if (todoItems.length === 0) list.innerHTML = '';
+    return
+  }
+
   const isChecked = todo.checked ? 'done': '';
 
   const node = document.createElement("li");
   node.setAttribute('class', `todo__item ${isChecked}`);
   node.setAttribute('data-key', todo.id);
-
   node.innerHTML = `
-    <input type="checkbox" id="${todo.id}" class="todo__complete todo__checkbox">
+    <input type="checkbox" id="${todo.id}" class="complete__todo todo__checkbox">
     <label for="${todo.id}"></label>
     <span class="todo__task">${todo.text}</span>
     <button class="delete__todo"></button>
   `;
 
-  list.append(node);
+  if (item) {
+    list.replaceChild(node, item);
+  } else {
+    list.append(node);
+  }
 }
 
+// добавить новое дело
 const addTodo = (text) => {
   const todo = {
     text,
@@ -32,6 +47,28 @@ const addTodo = (text) => {
   renderTodo(todo);
 }
 
+// отметить дело как выполненное
+const toggleDone = (key) => {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+// удалить дело
+const deleteTodo = (key) => {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+
+  const todo = {
+    deleted: true,
+    ...todoItems[index]
+  };
+
+  todoItems = todoItems.filter(item => item.id !== Number(key));
+  renderTodo(todo);
+}
+
+// слушател событий
 addButton.addEventListener('click', (event) => {
   event.preventDefault();
 
@@ -41,5 +78,26 @@ addButton.addEventListener('click', (event) => {
     input.value = '';
     input.focus();
   }
-  console.log(text);
 })
+
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('complete__todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  if (event.target.classList.contains('delete__todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ref = localStorage.getItem('todoItemsRef');
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach(t => {
+      renderTodo(t);
+    });
+  }
+});
