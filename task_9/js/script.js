6dcffae96a8f52da460444fff70b89eabbcce21c
Andrii Konovalenko <an256.ak@gmail.com>
2022-07-17 23:05:05 +0300

    feat: add dark theme

--- task_9/js/script.js
+++ task_9/js/script.js
@@ -3,7 +3,8 @@ const addButton = document.querySelector('.todo__add'),
   list = document.querySelector('.todo__items'),
   clearAllButton = document.querySelector('.clear__button'),
   itemsLeft = document.querySelector('.number-items'),
-  buttonsBlock = document.querySelector('.buttons__block');
+  buttonsBlock = document.querySelector('.buttons__block'),
+  toggleThemeButton = document.querySelector('.toggle-button');
 
 let todoItems = [];
 
@@ -74,6 +75,7 @@ const deleteTodo = (key) => {
   howItemsLeft();
 }
 
+// количесиво оставшихся дел
 const howItemsLeft = () => {
   let n = 0;
   for (let i = 0; i < todoItems.length; i++) {
@@ -85,6 +87,7 @@ const howItemsLeft = () => {
   itemsLeft.textContent = n;
 }
 
+// сортировка списка дел
 const sortTodo = (todo) => {
   const listItems = document.querySelectorAll('.todo__item');
 
@@ -118,6 +121,62 @@ const sortTodo = (todo) => {
     default:
       break;
   }
+}
+
+// изменение темы
+let darkTheme = false;
+let bodyColor = '#E3E4F1',
+  todoDark = '#E3E4F1',
+  white = '#ffffff',
+  todoTask = '#494C6B';
+  todoTaskCompleted = '#D1D2DA';
+  textDark = '#9495A5';
+
+const changeTheme = () => {
+  const header = document.querySelector('.header'),
+    checklistIcon = document.querySelector('.checklist-icon');
+  let root = document.documentElement;
+
+  // localStorage.setItem('darkTheme', darkTheme);
+
+  if (!darkTheme) {
+    localStorage.setItem('darkTheme', '');
+
+    toggleThemeButton.classList.remove('toggle-button-dark');
+    header.classList.remove('header-dark');
+    checklistIcon.src="img/to-do-list.png";
+
+    bodyColor = '#E3E4F1';
+    todoDark = '#E3E4F1';
+    white = '#ffffff';
+    todoTask = '#494C6B';
+    todoTaskCompleted = '#D1D2DA';
+    textDark = '#9495A5';
+
+    darkTheme = true;
+  } else {
+    localStorage.setItem('darkTheme', true);
+
+    toggleThemeButton.classList.add('toggle-button-dark');
+    header.classList.add('header-dark');
+    checklistIcon.src="img/to-do-list-white.png";
+
+    bodyColor = '#171823';
+    todoDark = '#979797';
+    white = '#25273D';
+    todoTask = '#C8CBE7';
+    todoTaskCompleted = '#4D5067';
+    textDark = '#5B5E7E';
+
+    darkTheme = false;
+  }
+
+  root.style.setProperty('--dark', bodyColor);
+  root.style.setProperty('--todo-dark', todoDark);
+  root.style.setProperty('--white', white);
+  root.style.setProperty('--todo-task', todoTask);
+  root.style.setProperty('--todo-task-c', todoTaskCompleted);
+  root.style.setProperty('--text-dark', textDark);
 }
 
 // слушатели событий
@@ -168,6 +227,8 @@ buttonsBlock.addEventListener('click', (e) => {
   }
 })
 
+toggleThemeButton.addEventListener('click', changeTheme);
+
 document.addEventListener('DOMContentLoaded', () => {
   const ref = localStorage.getItem('todoItemsRef');
   if (ref) {
@@ -176,5 +237,13 @@ document.addEventListener('DOMContentLoaded', () => {
       renderTodo(t);
     });
   }
+
   howItemsLeft();
+
+  if (localStorage.getItem('darkTheme') !== null) {
+    darkTheme = localStorage.getItem('darkTheme');
+  } else {
+    darkTheme = darkTheme;
+  }
+  changeTheme();
 });
