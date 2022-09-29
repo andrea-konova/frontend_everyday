42b5e548e7286909860d088d6352ef039855b63c
Andrii Konovalenko <an256.ak@gmail.com>
2022-09-29 12:19:09 +0300

    feat: get data from Api

--- task_11/src/components/v-catalog.vue
+++ task_11/src/components/v-catalog.vue
@@ -2,7 +2,7 @@
   <div class="v-catalog">
     <!-- <h2>Заголовок</h2> -->
     <v-catalog-item
-      v-for="product in products"
+      v-for="product in PRODUCTS"
       :key="product.article"
       :product_data="product"
       @sendName="showName"
@@ -12,6 +12,7 @@
 
 <script>
   import vCatalogItem from './v-catalog-item.vue';
+  import { mapActions, mapGetters } from 'vuex';
 
   export default {
     name: 'v-catalog',
@@ -21,91 +22,30 @@
     props: {},
     data() {
       return {
-        products: [
-        {
-          image: "p_1.png",
-          name: "Quattro formaggi",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_1",
-          dish: "pizza"
-        },
-        {
-          image: "p_2.png",
-          name: "Carbonara",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_2",
-          dish: "pizza"
-        },
-        {
-          image: "p_3.png",
-          name: "Philadelphia",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_3",
-          dish: "pizza"
-        },
-        {
-          image: "p_4.png",
-          name: "Di mare",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_4",
-          dish: "pizza"
-        },
-        {
-          image: "p_5.png",
-          name: "Paperoni",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_5",
-          dish: "pizza"
-        },
-        {
-          image: "p_6.png",
-          name: "Margarita",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_6",
-          dish: "pizza"
-        },
-        {
-          image: "p_7.png",
-          name: "Quattro formaggi",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_7",
-          dish: "pizza"
-        },
-        {
-          image: "p_8.png",
-          name: "Gawaiian",
-          diameter: "32 cм",
-          ingredients: "Dough, Mozzarella, Cheddar, Blue, Parmesan",
-          price: "119 UAH",
-          article: "p_8",
-          dish: "pizza"
-        }
-      ]
+        
       }
     },
-    computed: {},
+    computed: {
+      ...mapGetters([
+        'PRODUCTS'
+      ]),
+    },
     methods: {
+      ...mapActions([
+        'GET_PRODUCTS_FROM_API'
+      ]),
       showName(data) {
         alert(data);
       }
     },
     watch: {},
     mounted() {
-      console.log('Hello, v-catalog is alive!');
+      this.GET_PRODUCTS_FROM_API()
+      .then((response) => {
+        if (response.data) {
+          console.log('Данные для карточки товара получены');
+        }
+      })
     }
   }
 </script>
