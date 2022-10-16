9a52a7bd3f91d01f241a4daadb6c25d43a30158a
Andrii Konovalenko <54073347+andrea-konova@users.noreply.github.com>
2022-10-17 00:01:31 +0300

    Add task_11 folder

---
+++ task_11/js/about.711fd87a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktask_11"]=self["webpackChunktask_11"]||[]).push([[443],{5399:function(t,n,s){s.r(n),s.d(n,{default:function(){return i}});var u=function(){var t=this;t._self._c;return t._m(0)},a=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],e=s(1001),l={},r=(0,e.Z)(l,u,a,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.711fd87a.js.map
